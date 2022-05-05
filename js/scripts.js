493baff788780269b905cb5c995d4283737b06f2
SwapnaliTaware <104899864+SwapnaliTaware@users.noreply.github.com>
2022-05-05 18:12:54 +0530

    webtask

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+ const hamburger = document.querySelector(".ham");  
+ const navsub = document.querySelector(".new-list");  
+ hamburger.addEventListener('click', () => {  
+  hamburger.classList.toggle("change")  
+  navsub.classList.toggle("new-list")  
+ });
+
+
+
+
+$(document).ready(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    directionNav: false,
+    controlNav: true,
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
